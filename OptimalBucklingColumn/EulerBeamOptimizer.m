e857900689c7c3df2b48dcc15217a49055acd4bd
aitzibermaneru <91916944+aitzibermaneru@users.noreply.github.com>
2022-02-11 13:22:59 +0100

    Add files via upload
    Refactoring

--- OptimalBucklingColumn/EulerBeamOptimizer.m
+++ OptimalBucklingColumn/EulerBeamOptimizer.m
@@ -1,6 +1,5 @@
 classdef EulerBeamOptimizer < handle
     
-    
     properties (Access = private)
         
     end
@@ -11,6 +10,9 @@ classdef EulerBeamOptimizer < handle
         length
         youngModulus
         inertiaMoment
+        minThick
+        maxThick
+        optimizerType
     end
     
     methods (Access = public)
@@ -25,50 +27,173 @@ classdef EulerBeamOptimizer < handle
     methods (Access = private)
         
         function init(obj)
-            obj.nElem        = 100;
-            obj.nConstraints = 3;
-            obj.length       = 1/obj.nElem;
+            obj.nElem        = 10;
+            obj.nConstraints = 3; % m
+            obj.length       = 1/obj.nElem; 
             obj.youngModulus = 1;
-            obj.inertiaMoment = 1;            
+            obj.inertiaMoment = 1;  
+            obj.minThick      = 0.25;
+            obj.maxThick      = 10;
+            obj.optimizerType = 'MMA';
         end
         
-        function Be = computeBendingMatrix(obj)
+        function Be = computeElementalBendingMatrix(obj)
             L = obj.length;
             E = obj.youngModulus;
             I = obj.inertiaMoment;
-            Be =(E*I/(L^3))*[12 6*L -12 6*L;  6*L 4*L^2 -6*L 2*L^2; -12 -6*L 12 -6*L; 6*L 2*L^2 -6*L 4*L^2];
+            [c1,c2,c3,c4] = obj.coeffsBending(L,E,I);
+            Be(1,1:4) = c1*[c2 c3 -c2 c3];
+            Be(2,1:4) = c1*[c3 c4 -c3 c4/2];
+            Be(3,1:4) = c1*[-c2 -c3 c2 -c3];
+            Be(4,1:4) = c1*[c3 c4/2 -c3 c4];
         end
-        
-        function Ke = computeStiffnessMatrix(obj)
-            L = obj.length;                     
-            Ke = 1/(30*L)*[36 3*L -36 3*L; 3*L 4*L^2 -3*L -L^2; -36 -3*L 36 -3*L; 3*L -L^2 -3*L 4*L^2];                        
+
+        function [c1,c2,c3,c4] = coeffsBending(obj,L,E,I)
+            c1 = E*I/L^3;
+            c2 = 12;
+            c3 = 6*L;
+            c4 = 4*L^2;
         end
-        
-        function [x] = bound(obj)
+
+        function Ke = computeElementalStiffnessMatrix(obj)
+            L = obj.length;       
+            [c1,c2,c3,c4,c5] = obj.coeffsStiffness(L);
+            Ke(1,1:4) = c1*[c2 c3 -c2 c3];
+            Ke(2,1:4) = c1*[c3 c4 -c3 -c5];
+            Ke(3,1:4) = c1*[-c2 -c3 c2 -c3];
+            Ke(4,1:4) = c1*[c3 -c5 -c3 c4];
+        end
+
+        function [c1,c2,c3,c4,c5] = coeffsStiffness(obj,L)
+            c1 = 1/(30*L);
+            c2 = 36;
+            c3 = 3*L;
+            c4 = 4*L^2;
+            c5 = L^2;
+        end
+
+        function B = computeBendingMatrix(obj,Be,x)
             N = obj.nElem;
-            % This algorithm maximizes the least eigenvalue of a clamped-clamped
-            % column, accounting for the presence of simple or multiple eigenvalues.
-            % It also illustrates the best strongest profile of that column against
-            % buckling as well as it gives its buckling modes. Its input variable is N,
-            % which refers to the number of elements of column with the Finite Element
-            % Analysis.
-            
-            
-            % CONSTANTS DEFINITION
+            B=sparse(2*N+2, 2*N+2);    
+            for iElem = 1: N
+                iDof=[2*iElem-1; 2*iElem; 2*(iElem+1)-1; 2*(iElem+1)];
+                B(iDof,iDof)=B(iDof,iDof)+(x(iElem)^2)*Be;
+            end
+        end
 
-            L=1/N ;         %Element's length
+        function K = computeStiffnessMatrix(obj,Ke)
+            N = obj.nElem;
+            K = sparse(2*N+2, 2*N+2);     
+            for iElem = 1: N
+                iDof=[2*iElem-1; 2*iElem; 2*(iElem+1)-1; 2*(iElem+1)];
+                K(iDof,iDof)=K(iDof,iDof)+ Ke;
+            end
+        end
 
-            
-            % VARIABLES DEFINITION
+        function  [fixnodes,nodes,freenodes] = computeBoundaryConditions(obj)
+            N = obj.nElem;
+            fixnodes = union([1,2], [2*N+1,2*N+2]);
+            nodes      = 1:2*N+2;
+            freenodes= setdiff(nodes,fixnodes);
+        end
+
+        function [x] = bound(obj)  
+            N = obj.nElem;
+            L = obj.length;
             n_val=N+1;
             m = obj.nConstraints;
+            % LIMITATIONS OF THE DESIGN 
+            [x,xmin,xmax,xold1,xold2,loop,low,upp,a0,a_mma,d,c] = obj.computeVariablesMMA(n_val,m);
+            % AUXILIAR VECTORS DEFINITION FOR EIGENVALUES COMPARATIVE
+            e=zeros(loop);
+            E1=zeros(loop);
+            E2=zeros(loop);
+            % ELEMENTAL MATRICES
+            Be = obj.computeElementalBendingMatrix();
+            Ke = obj.computeElementalStiffnessMatrix();
             
+            % ITERATIVE PROCESS
+            change = 1;
+            while (change > 0.0005) & (loop < 1000)
+                loop = loop + 1;
+                % B AND K MATRICES ASSEMBLY
+                B = obj.computeBendingMatrix(Be,x);
+                K = obj.computeStiffnessMatrix(Ke);
+                % BOUNDARY CONDITIONS
+                [~,~,freenodes] = obj.computeBoundaryConditions();
+                % EIGENVALUES AND EIGENVECTORS
+                [V,D,v1,v2,lambda] = obj.computeEigenModesAndValues(freenodes,K,B);
+
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %__________________MMA____________________
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                % todo esto lo calcula la clase hija, hay que llamarla
+                % desde aqui.
+
+                %% necesita todo esto 
+
+%             s.type = obj.optimizerType;
+%             s.designVariable = x;
+%             obj.constraintCase    = cParams.constraintCase;
+%             obj.cost              = cParams.cost;
+%             obj.constraint        = cParams.constraint;
+%             obj.designVariable    = cParams.designVar;
+%             obj.dualVariable      = cParams.dualVariable;
+%             obj.maxIter           = cParams.maxIter;
+%             obj.incrementalScheme = cParams.incrementalScheme;
+%             obj.targetParameters  = cParams.targetParameters;
+
+%             s.type = obj.optimizerType;
+                %%
+ 
+
+             
+                % UPDATING OF THE SOLUTION
+                xval = x; % design variable -----> será el input al optimizier_MMA
+                % OBJECTIVE FUNCTION
+                [f0val,df0dx,df0dx2] = obj.computeCost(x);  % ya lo calcula lo otro
+                % CONSTRAINTS 
+                fval=obj.computeConstraintFunction(x,lambda);  % ya lo calcula
+                [dfdx,dfdx2] = obj.computeConstraintDerivative(Be,D,x,v1,v2,V); % ya lo calcula
+                % INVOKING MMA
+                % obj.computeNewDesign()
+                [xmma,~,~,~,~,~,~,~,~,low,upp] = ...
+                    mmasub(m,n_val,loop,xval,xmin,xmax,xold1,xold2, ...
+                    f0val,df0dx,df0dx2,fval,dfdx,dfdx2,low,upp,a0,a_mma,c,d);
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+                % BUCKLING MODES
+                [Mode1,Mode2] = obj.computeBucklingModes(v1,v2);
+                % CONVERGENCE DOUBLES EIGENVLAUES
+                [e,E1,E2]= obj.computeConvergence2Eigenvalues(loop,D,e,E1,E2);
+                % PLOTTING
+                obj.plot(Mode1,Mode2,e,E1,E2,x)
+                %OUTPUT VARIABLES UPDATING
+                xold2 = xold1;
+                xold1 = xval;
+                x = xmma;
+                change = max(abs(x-xold1));
+                % PRINTING OF THE RESULTS
+                obj.displayIteration(loop,x,f0val,D)
+                % PLOTTING, COST AND VOLUME
+                cost(loop) = -xmma(N+1);
+                vol(loop) = (1/N)*sum(x(1:N));  % x=xmma, esta bien?
+                figure(3)
+                plot(cost)
+                figure(4)
+                plot(vol)
+
+            end
+   
+        end
+
+        function [x,xmin,xmax,xold1,xold2,loop,low,upp,a0,a_mma,d,c] = computeVariablesMMA(obj,n_val,m)
             % PUNCTUAL LIMITATIONS OF THE DESIGN
             %(Pre-defined in MMA file)
-            alpha=0.25;
-            beta=10;
+            alpha = obj.minThick;
+            beta = obj.maxThick;
+            N = obj.nElem;
             x=ones(N+1,1);
-            
             xmin=alpha*ones(N,1);
             xmin=[xmin; 0];
             xmax=beta*ones(N,1);
@@ -83,106 +208,12 @@ classdef EulerBeamOptimizer < handle
             a_mma = zeros(m,1);
             d = zeros(m,1);
             c = 1000*ones(m,1);
-            
-            % AUXILIAR VECTORS DEFINITION FOR EIGENVALUES COMPARATIVE
-            e=zeros(loop);
-            E1=zeros(loop);
-            E2=zeros(loop);
-            
-            Be = obj.computeBendingMatrix();
-            Ke = obj.computeStiffnessMatrix();
-            
-            % ITERATIVE PROCESS
-            change = 1;
-            while (change > 0.0005) & (loop < 1000)
-                loop = loop + 1;
-                
-                % BENDING AND STIFFNESS MATRICES DEFINTION
-                B=sparse(2*N+2, 2*N+2);
-                K=sparse(2*N+2, 2*N+2);
-                
-                % BENDING AND STIFFNESS MATRICES ASSEMBLY
-                for elx=1:N
-                    edof=[2*elx-1; 2*elx; 2*(elx+1)-1; 2*(elx+1)];
-                    B(edof,edof)=B(edof,edof)+(x(elx)^2)*Be;
-                    K(edof,edof)=K(edof,edof)+ Ke;
-                end
-                
-                % BOUNDARY CONDITIONS
-                fixnodes = union([1,2], [2*N+1,2*N+2]);
-                nodes      = 1:2*N+2;
-                freenodes= setdiff(nodes,fixnodes);
-                
-                [V,D,v1,v2,lambda] = obj.computeEigenModesAndValues(freenodes,K,B);
-                
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                %__________________MMA____________________
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                
-                % UPDATING OF THE SOLUTION
-                xval = x;
-                
-                % OBJECTIVE FUNCTION
-                [f0val,df0dx,df0dx2] = obj.computeCost(x);
 
-                
-                % CONSTRAINTS VECTOR
-                fval=obj.computeConstraintFunction(x,lambda);
-                
-                [dfdx,dfdx2] = obj.computeConstraintDerivative(Be,D,x,v1,v2,V);
-                
-                % INVOKING MMA
-%                obj.computeNewDesign()
-                [xmma,~,~,~,~,~,~,~,~,low,upp] = ...
-                    mmasub(m,n_val,loop,xval,xmin,xmax,xold1,xold2, ...
-                    f0val,df0dx,df0dx2,fval,dfdx,dfdx2,low,upp,a0,a_mma,c,d);
-                
-                
-                %CALCULATION THE OF BUCKLING MODES
-                Mode1=zeros(2*N+2);
-                Mode2=zeros(2*N+2);
-                
-                for i=3:2*N
-                    Mode1(i)=v1(i-2);
-                    Mode2(i)=v2(i-2);
-                end
-                
-                % COLUMN'S PROFILE
-                z=sqrt(x(1:N));
-
-                % CONVERGENCE OF DOUBLE EIGENVALUES
-                e(loop)=loop;
-                E1(loop)= D(1,1);
-                E2(loop)=D(2,2);                
-                
-                obj.plot(z,Mode1,Mode2,e,E1,E2)
-                %OUTPUT VARIABLES UPDATING
-                xold2 = xold1;
-                xold1 = xval;
-                
-                x = xmma;
-                change = max(abs(x-xold1));
-                
-                
-                % PRINTING OF THE RESULTS
-                obj.displayIteration(loop,x,f0val,D)
-
-                cost(loop) = -xmma(N+1);
-                vol(loop) = (1/N)*sum(x(1:N));
-                figure(3)
-                plot(cost)
-                figure(4)
-                plot(vol)
-            end
-
-            
         end
 
         function [V,D,v1,v2,lambda] = computeEigenModesAndValues(obj,freenodes,K,B)
             [V,D]=eigs(B(freenodes,freenodes),K(freenodes,freenodes),2,'SM');
-
             lambda=sort(diag(D));
-
             if lambda(1)==D(1,1)
                 v1=V(:,1);
                 v2=V(:,2);
@@ -190,7 +221,6 @@ classdef EulerBeamOptimizer < handle
                 v1=V(:,2);
                 v2=V(:,1);
             end
-
         end
 
         function [f0val,df0dx,df0dx2] = computeCost(obj,x)
@@ -199,12 +229,9 @@ classdef EulerBeamOptimizer < handle
             % OBJECTIVE FUNCTION'S FIRST DERIVATIVE
             df0dx=zeros(N+1,1);
             df0dx(N+1)=-1;
-
             % OBJECTIVE FUNCTION'S SECOND DERIVATIVE
             df0dx2 = 0*df0dx;
         end
-
-
 
         function fx = computeConstraintFunction(obj,x,lambda)
             N = obj.nElem;
@@ -220,8 +247,6 @@ classdef EulerBeamOptimizer < handle
                 
                 % CONSTRAINTS VECTOR'S SECOND DERIVATIVE
                 dfdx2 = 0*dfdx;
-                
-                
                 
                 % MULTIPLE EIGENVALUE'S IDENTIFICATION
                 if abs(D(2,2)-D(1,1))> 1
@@ -303,19 +328,38 @@ classdef EulerBeamOptimizer < handle
                 ' ch.: ' sprintf('%6.3f',abs(D(2,2)-D(1,1)) )])
             
         end
-        
-         function plot(obj,z,Mode1,Mode2,e,E1,E2)
+
+        function [Mode1,Mode2] = computeBucklingModes(obj,v1,v2)
+            N = obj.nElem;
+            %CALCULATION THE OF BUCKLING MODES
+            Mode1=zeros(2*N+2);
+            Mode2=zeros(2*N+2);
+            for i=3:2*N
+                Mode1(i)=v1(i-2);
+                Mode2(i)=v2(i-2);
+            end
+        end
+
+        function [e,E1,E2]= computeConvergence2Eigenvalues(obj,loop,D,e,E1,E2)
+            % CONVERGENCE OF DOUBLE EIGENVALUES
+            e(loop)=loop;
+            E1(loop)= D(1,1);
+            E2(loop)=D(2,2);
+        end
+
+        function plot(obj,Mode1,Mode2,e,E1,E2,x)
                 N = obj.nElem;
                 L = obj.length;
                 % AXES DEFINION FOR FIGURES
                 ch= 0:L:1-L;
-                h= 0:L:1;
-                
-                
+                h= 0:L:1;       
+
+                % COLUMN'S PROFILE
+                z=sqrt(x(1:N));
+
                 % PLOT OF THE BEST STRONGEST PROFILE OF THE COLUMN AGAINST BUCKLING
                 % Clamped-clamped configuration
                 figure(1)
-                
                 subplot(2,2,[1 3]);plot(ch,z)
                 title('Clamped-Clamped Column Profile','Interpreter', 'latex','FontSize',20, 'fontweight','b');
                 xlabel('x','Interpreter', 'latex','fontsize',14,'fontweight','b');
@@ -327,9 +371,7 @@ classdef EulerBeamOptimizer < handle
                 
                 subplot(2,2,4); plot(h,-Mode2(1:2:2*N+2));
                 title('Second Buckling Mode','Interpreter', 'latex','FontSize',14, 'fontweight','b')
-                
-
-                
+                    
                 figure(2)
                 hold on                
                 plot(e,E1);
@@ -338,8 +380,7 @@ classdef EulerBeamOptimizer < handle
                 xlabel('Number of Iteration','Interpreter', 'latex','fontsize',18,'fontweight','b');
                 ylabel('Eigenvalues','Interpreter', 'latex','fontsize',18,'fontweight','b');
                 
-                axis([0 65 0 100]);
-                                
+                axis([0 65 0 100]);                             
             end        
         
     end
